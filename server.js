42b4330ed7946881a0d9260b9de1457caba2b092
huongse7en <huongse7en>
2020-10-17 03:17:03 +0700

    add lib

--- server.js
+++ server.js
@@ -2,10 +2,8 @@
 const path = require("path");
 const express = require("express");
 const app = express(); // create express app
-
+var history = require('connect-history-api-fallback');
+app.use(history());
 app.use(express.static("public"));
-app.get('*', function (req, res) {
-    return res.sendFile('public/index.html');
-});
 // start express server on port 80
 app.listen(process.env.PORT || 4000);
