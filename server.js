387de13f078c2df991117dc0b079c9e15e5354a2
huongse7en <huongse7en>
2020-10-17 03:09:08 +0700

    TesT

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express(); // create express app
 
 app.use(express.static("public"));
-
+app.get('*', function (req, res) {
+    return res.sendFile('public/index.html');
+});
 // start express server on port 80
 app.listen(process.env.PORT || 4000);
